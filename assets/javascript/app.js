a4777f5742881e44660cc3e96f55dadc29089226
JaniceNova <janicenovacek2016@gmail.com>
2018-11-20 15:09:39 -0800

    first draft

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,33 @@
 var topics = ["flapjack", "totally spies", "teen titains", "dave the barbarian", "Foster's Home for Imaginary Friends", "regular show", "danny phantom", "fairly odd parents", "digimon", "yugioh"]
 
-
-
-
-
 for (var i = 0; i < topics.length; i++) {
 
     $("#divforbuttons").append("<button>" + topics[i] + "</button>")
 
 };
 
+$("#addGif").on("click", function (e) {
+    e.preventDefault();
+    var cartoonName = $("#cartoonButton").val();
+    console.log(cartoonName);
+    topics.push(cartoonName);
+    $("#divforbuttons").empty();
+    console.log(topics);
+    for (var i = 0; i < topics.length; i++) {
+        $("#divforbuttons").append("<button>" + topics[i] + "</button>")
+    };
 
-$("button").on("click", function () {
+
+    
+})
+
+
+
+
+
+
+
+$(document).on("click", "button", function () {
 
     var cartoon = $(this).text();
     console.log(cartoon);
@@ -32,21 +48,21 @@ $("button").on("click", function () {
 
 
             $("#giffs").append("<img src='" + result[i].images.fixed_height_small_still.url + "' data-still='" + result[i].images.fixed_height_small_still.url + "'data-animate='" + result[i].images.fixed_height_small.url + "'data-state='still' class='gif'>" + "<p>Rating: " + result[i].rating + "</p><br><br>")
-
+            
 
         };
 
 
-        
 
     });
+
+
+
 });
 
-$("#addGif").on("click", function (e) {
-    e.preventDefault();
-    var cartoonName = $("#cartoonButton").val();
-    console.log(cartoonName);
-});
+
+
+
 
 $(document).on("click", ".gif", function () {
 
