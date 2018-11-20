72881947c1f657851db3779afb5a2e0d0c5fc175
JaniceNova <janicenovacek2016@gmail.com>
2018-11-20 09:17:02 -0800

    added onclick function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,38 +4,54 @@ var topics = ["flapjack", "totally spies", "teen titains", "dave the barbarian",
 
 
 
-for (var i=0; i<topics.length; i++){
-    
+for (var i = 0; i < topics.length; i++) {
+
     $("#divforbuttons").append("<button>" + topics[i] + "</button>")
 
 };
 
 
-$("button").on("click", function() {
+$("button").on("click", function () {
 
-var cartoon = $(this).text();
-console.log(cartoon);
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-cartoon + "&api_key=7Ok8EeQzwrvBSD0j30NJFdbtQybGJMZj&limit=10";
-console.log(queryURL);
+    var cartoon = $(this).text();
+    console.log(cartoon);
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        cartoon + "&api_key=7Ok8EeQzwrvBSD0j30NJFdbtQybGJMZj&limit=10";
+    console.log(queryURL);
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response){
-console.log(response)
-var result = response.data;
-console.log(result);
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response)
+        var result = response.data;
+        console.log(result);
 
-$("#giffs").empty();
-for (var i=0; i<result.length; i++){
-    
-
-$("#giffs").append("<img src='" + result[i].images.fixed_height_small_still.url + "' data-still='" +  result[i].images.fixed_height_small_still.url +"'data-animate='" ">" + "<p>Rating: "+ result[i].rating+ "</p><br><br>")
+        $("#giffs").empty();
+        for (var i = 0; i < result.length; i++) {
 
 
-};
+            $("#giffs").append("<img src='" + result[i].images.fixed_height_small_still.url + "' data-still='" + result[i].images.fixed_height_small_still.url + "'data-animate='" + result[i].images.fixed_height_small.url + "'data-state='still' class='gif'>" + "<p>Rating: " + result[i].rating + "</p><br><br>")
 
 
-});
+        };
+
+        $(".gif").on("click", function () {
+
+            var state = $(this).attr("data-state");
+
+            var animate = $(this).attr("data-animate")
+            var still = $(this).attr("data-still")
+
+            if (state === 'still') {
+                $(this).attr("src", animate);
+                $(this).attr("data-state", "animate");
+            }
+            else {
+                $(this).attr("src", still);
+                $(this).attr("data-state", "still");
+            }
+        });
+
+    });
 });
